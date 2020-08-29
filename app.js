fe00d014461af3983d4f1eed5bd0befbd27bfd9f
Ashwin <ashwinsuresh.x@gmail.com>
2020-08-29 22:18:21 +0530

    Initial Commit

---
+++ app.js
@@ -0,0 +1,34 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const app = express();
+
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+app.use(express.static("public"));
+app.set("view engine", "ejs");
+
+app.get("/", function(req, res) {
+  res.render("home", {
+    pageTitle: "Ashwin"
+  });
+})
+
+app.get("/about", function(req, res) {
+  res.render("about", {
+    pageTitle: "About-Ashwin"
+  });
+})
+
+app.get("/contact", function(req, res) {
+  res.render("contact", {
+    pageTitle: "Contact-Ashwin"
+  });
+})
+
+
+
+
+app.listen(8899, function() {
+  console.log("Server running Successfully");
+})
