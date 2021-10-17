34816d09bcf664cb8f4646f11f5b2aed4ed2a361
archillexx <ashwinsuresh.x@gmail.com>
2021-10-17 17:17:41 +0530

    Add Sawo Login(Testing)

--- app.js
+++ app.js
@@ -1,34 +1,39 @@
-const express = require("express");
-const bodyParser = require("body-parser");
-const app = express();
+const express = require('express')
+const bodyParser = require('body-parser')
+const app = express()
 
-app.use(bodyParser.urlencoded({
-  extended: true
-}));
-app.use(express.static("public"));
-app.set("view engine", "ejs");
+app.use(
+	bodyParser.urlencoded({
+		extended: true,
+	})
+)
+app.use(express.static('public'))
+app.set('view engine', 'ejs')
 
-app.get("/", function(req, res) {
-  res.render("home", {
-    pageTitle: "Ashwin"
-  });
+app.get('/', function (req, res) {
+	res.render('home', {
+		pageTitle: 'Ashwin',
+	})
 })
 
-app.get("/about", function(req, res) {
-  res.render("about", {
-    pageTitle: "About-Ashwin"
-  });
+app.get('/about', function (req, res) {
+	res.render('about', {
+		pageTitle: 'About-Ashwin',
+	})
 })
 
-app.get("/contact", function(req, res) {
-  res.render("contact", {
-    pageTitle: "Contact-Ashwin"
-  });
+app.get('/contact', function (req, res) {
+	res.render('contact', {
+		pageTitle: 'Contact-Ashwin',
+	})
 })
 
+app.get('/login', function (req, res) {
+	res.render('login', {
+		pageTitle: 'Sawo-Login',
+	})
+})
 
-
-
-app.listen(process.env.PORT || 8899, function() {
-  console.log("Server running Successfully");
+app.listen(process.env.PORT || 8899, function () {
+	console.log('Server running Successfully')
 })
